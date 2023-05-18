8320752f13bb68efd953d8478e0b5161884f7907
GhenadieBalan <ghennadiebalan@gmail.com>
2023-05-18 20:09:28 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nume="Ghenadie";
+        String prenume="Balan";
+        String functia="Началник свежего воздуха!";
+        System.out.println (nume +" "+ prenume +" "+ functia + " Maib" );
+    }
+}
