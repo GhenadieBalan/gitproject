c31be05ddbe21cd5219b42428ac3ba687ff14350
GhenadieBalan <ghennadiebalan@gmail.com>
2023-05-19 23:10:17 +0300

    tema pe acasa

---
+++ src/temaPeAcasa.java
@@ -0,0 +1,15 @@
+public class temaPeAcasa {
+    public static void main(String[] args) {
+        String fraza = "i love java";
+        System.out.println(fraza + " and QA Automation");
+        int numar = fraza.length();
+        System.out.println(numar);
+        char caracter = fraza.charAt(4);
+        System.out.println(caracter);
+        int index = fraza.indexOf('j');
+        System.out.println(index);
+        boolean string = fraza instanceof String;
+        System.out.println(string);
+    }
+}
+
